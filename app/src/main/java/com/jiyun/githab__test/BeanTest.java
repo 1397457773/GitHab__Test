dfb9f98d3341b0c1b2ebb7b52cdfee42244c4bf5
1397457773 <1397457773@qq.com>
2017-08-14 18:59:51 +0800

    第七次提交 提供Test类set/get方法
    时间：2017年8月14日18:58:45
    程序员：李俊岐

--- app/src/main/java/com/jiyun/githab__test/BeanTest.java
+++ app/src/main/java/com/jiyun/githab__test/BeanTest.java
@@ -13,4 +13,28 @@ public class BeanTest {
     private void newMethod(){
         Log.e("TAG","Test");
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
 }
