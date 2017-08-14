ae68a4dd62b0c4f56d36f02c391c7ceabacbaf27
1397457773 <1397457773@qq.com>
2017-08-14 18:30:15 +0800

    第三次提交 提供一个测试类
    时间：2017年8月14日18:30:10
    程序员：李俊岐

---
+++ app/src/main/java/com/jiyun/githab__test/BeanTest.java
@@ -0,0 +1,13 @@
+package com.jiyun.githab__test;
+
+import android.util.Log;
+
+/**
+ * Created by Lenovo on 2017/8/14.
+ */
+
+public class BeanTest {
+    private void newMethod(){
+        Log.e("TAG","Test");
+    }
+}
